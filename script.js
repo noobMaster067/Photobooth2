02fd14bff6234980d5c1178420fc78114c26003d
noobMaster067 <qzf2dbgmrd@privaterelay.appleid.com>
2026-01-31 14:40:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function drawTemplate() {
   ctx.drawImage(template, 0, 0, canvas.width, canvas.height);
 
   const slots = [
-    { x: 125, y: 123, w: 940, h: 940},
+    { x: 100, y: 123, w: 940, h: 940},
     { x: 126, y: 1173, w: 940, h: 940},
     { x: 126, y: 2223, w: 940, h: 940}
   ];
